8c553aaacff52c8fb4b8d10fc40d6b2241836041
looser <nitin23329@gmail.com>
2020-07-22 16:19:19 +0530

    fixed a bug solved the base cases printing
    fixed another bug : solved the negative input boundary case

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,14 @@
 import sys
 
-n = int(input('enter upto which you want to see finbonacci series'))
+n = int(input('enter upto which you want to see finbonacci series: '))
+if n<1:
+    print('please enter a valid number')
+    sys.exit(-1)
+print(0)
 if n==1:
-    print(0)
     sys.exit(0)
+print(1)
 if n<=2:
-    print(1)
     sys.exit(0)
 f = 0
 s = 1
